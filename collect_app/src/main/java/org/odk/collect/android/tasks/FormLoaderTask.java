4757f946efb2f13a8004c4eb5ce571253fa4366c
Hélène Martin <lognaturel@gmail.com>
2021-09-21 09:02:32 -0700

    Run string replace script

--- collect_app/src/main/java/org/odk/collect/android/tasks/FormLoaderTask.java
+++ collect_app/src/main/java/org/odk/collect/android/tasks/FormLoaderTask.java
@@ -125,7 +125,7 @@ public class FormLoaderTask extends AsyncTask<String, String, FormLoaderTask.FEC
         final String formPath = path[0];
         if (formPath == null) {
             Timber.e("formPath is null");
-            errorMsg = "formPath is null, please email support@getodk.org with a description of what you were doing when this happened.";
+            errorMsg = "formPath is null, please email support@kobotoolbox.org with a description of what you were doing when this happened.";
             return null;
         }
 
@@ -142,7 +142,7 @@ public class FormLoaderTask extends AsyncTask<String, String, FormLoaderTask.FEC
             errorMsg = TranslationHandler.getString(Collect.getInstance(), R.string.too_complex_form);
         } catch (Exception | Error e) {
             Timber.w(e);
-            errorMsg = "An unknown error has occurred. Please ask your project leadership to email support@getodk.org with information about this form.";
+            errorMsg = "An unknown error has occurred. Please ask your project leadership to email support@kobotoolbox.org with information about this form.";
             errorMsg += "\n\n" + e.getMessage();
         }
 
@@ -327,7 +327,7 @@ public class FormLoaderTask extends AsyncTask<String, String, FormLoaderTask.FEC
                     } else {
                         // The saved instance is corrupted.
                         Timber.e(e, "Corrupt saved instance");
-                        throw new RuntimeException("An unknown error has occurred. Please ask your project leadership to email support@getodk.org with information about this form."
+                        throw new RuntimeException("An unknown error has occurred. Please ask your project leadership to email support@kobotoolbox.org with information about this form."
                             + "\n\n" + e.getMessage());
                     }
                 }
